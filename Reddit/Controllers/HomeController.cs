0300a73f92109091af80da523c6bd85f05810cb7
AngelKrastev96 <angelkrustev@abv.bg>
2019-03-22 19:16:23 +0200

    Changes to HomeController

--- Reddit/Controllers/HomeController.cs
+++ Reddit/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using DataAccess.Repositories;
+﻿using DataAccess.Entities;
+using DataAccess.Repositories;
+using Reddit.Models;
 using Reddit.ViewModels.Home;
 using System;
 using System.Collections.Generic;
@@ -24,13 +26,62 @@ namespace Reddit.Controllers
         [HttpPost]
         public ActionResult Login(LoginVM model)
         {
+            if (this.ModelState.IsValid)
+            {
+                AuthenticationManager.Authenticate(model.Username, model.Password);
+
+                if (AuthenticationManager.LoggedUser == null)
+                    ModelState.AddModelError("authenticationFailed", "Wrong username or password!");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Logout()
+        {
+            AuthenticationManager.Logout();
+
+            return RedirectToAction("Login", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View(new RegisterVM());
+        }
+
+
+        [HttpPost]
+        public ActionResult Register(RegisterVM model)
+        {
             if (!ModelState.IsValid)
             {
                 return View(model);
             }
 
             UsersRepository repo = new UsersRepository();
-            return RedirectToAction("Index");
+
+            if (repo.GetAll(null).FirstOrDefault(m => m.Username == model.Username) != null)
+            {
+                ModelState.AddModelError("RegistrationFailed", "This username already exists !");
+                return View(model);
+            }
+
+            User item = new User();
+            item.Email = model.Email;
+            item.Username = model.Username;
+            item.Password = model.Password;
+            item.FirstName = model.FirstName;
+            item.LastName = model.LastName;
+            repo.Insert(item);
+
+            return RedirectToAction("Index", "Home");
+
+
         }
     }
 }
---
+++ Reddit/Models/AuthenticationManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataAccess.Entities;
+using DataAccess.Services;
+
+namespace Reddit.Models
+{
+    public class AuthenticationManager
+    {
+        public static User LoggedUser
+        {
+            get
+            {
+                AuthenticationService authenticationService = null;
+
+                if (HttpContext.Current != null && HttpContext.Current.Session["LoggedUser"] == null)
+                    HttpContext.Current.Session["LoggedUser"] = new AuthenticationService();
+
+                authenticationService = (AuthenticationService)HttpContext.Current.Session["LoggedUser"];
+                return authenticationService.LoggedUser;
+            }
+        }
+
+        public static void Authenticate(string username, string password)
+        {
+            AuthenticationService authenticationService = null;
+
+            if (HttpContext.Current != null && HttpContext.Current.Session["LoggedUser"] != null)
+                HttpContext.Current.Session["LoggedUser"] = new AuthenticationService();
+
+            authenticationService = (AuthenticationService)HttpContext.Current.Session["LoggedUser"];
+            authenticationService.AuthenticateUser(username, password);
+        }
+
+        public static void Logout()
+        {
+            HttpContext.Current.Session["LoggedUser"] = null;
+        } 
+    }
+}
