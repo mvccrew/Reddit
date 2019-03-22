ec9395ada04ed7e707f36c42c8f9cdf0eb5b854d
AngelKrastev96 <angelkrustev@abv.bg>
2019-03-22 18:36:15 +0200

    Added HomeController and ViewModels

---
+++ Reddit/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.Repositories;
+using Reddit.ViewModels.Home;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reddit.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View(new LoginVM());
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            UsersRepository repo = new UsersRepository();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Reddit/ViewModels/Home/RegisterVM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Reddit.ViewModels.Home
+{
+    public class RegisterVM
+    {
+        [EmailAddress]
+        [DisplayName("Email:")]
+        [Required(ErrorMessage = "This field is Required!")]
+        public string Email { get; set; }
+
+
+        [DisplayName("Username:")]
+        [Required(ErrorMessage = "This field is Required!")]
+        public string Username { get; set; }
+        [DisplayName("Password:")]
+        [Required(ErrorMessage = "This field is Required!")]
+        public string Password { get; set; }
+        [Required]
+        [System.ComponentModel.DataAnnotations.Compare("Password")]
+        public string RetypePassword { get; set; }
+        [DisplayName("First Name:")]
+        [Required(ErrorMessage = "This field is Required!")]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name:")]
+        [Required(ErrorMessage = "This field is Required!")]
+        public string LastName { get; set; }
+    }
+}
