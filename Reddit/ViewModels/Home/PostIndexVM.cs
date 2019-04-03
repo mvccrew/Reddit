0951f813a61892fc8e42b0e5b9ce85f289238d85
Yovcho <ivelinjilov16@gmail.com>
2019-04-03 16:38:00 +0300

    Show posts from subreddits on Home Index + a little CSS

---
+++ Reddit/ViewModels/Home/PostIndexVM.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reddit.ViewModels.Home
+{
+    public class PostIndexVM
+    {
+        public List<Post> Posts { get; set; }
+        public List<DataAccess.Entities.SubReddit> SubReddits { get; set; }
+    }
+}
