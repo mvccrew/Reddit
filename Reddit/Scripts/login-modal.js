0e7f872551d12a9a66fa8676f50a89568c06326c
Daniel Ivanov <ducciito@hotmail.com>
2019-04-05 01:38:07 +0300

    логин модала работи идеално, остана и за регистър да добавя

---
+++ Reddit/Scripts/login-modal.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+
+    const login = document.getElementById("login");
+    const closeLinks = document.querySelectorAll("[href='#close']");
+
+    login.addEventListener('click', () => {
+        const loginModal = document.getElementById("loginModal");
+        loginModal.classList.add('active');
+    });
+
+    if (closeLinks.length > 0) {
+        closeLinks.forEach(el => {
+            el.addEventListener('click', () => {
+                const loginModal = document.getElementById("loginModal");
+                loginModal.classList.remove('active');
+            });
+        });
+    }
+
+});
