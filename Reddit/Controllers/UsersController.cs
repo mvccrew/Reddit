8d07a06381ea0332737233c5839ace16bdeb8159
Daniel Ivanov <ducciito@hotmail.com>
2019-04-18 20:56:48 +0300

    edit modal for users

--- Reddit/Controllers/UsersController.cs
+++ Reddit/Controllers/UsersController.cs
@@ -13,5 +13,37 @@ namespace Reddit.Controllers
     [AuthenticationFilter(AdminArea = true, RequiredKarma = int.MinValue)]
     public class UsersController : BaseController<User, UsersRepository, FilterVM, IndexVM, EditVM>
     {
+        [HttpGet]
+        public override ActionResult Edit(int? id)
+        {
+            User item = null;
+
+            UsersRepository repo = new UsersRepository();
+            item = id == null ? new User() : repo.GetById(id.Value);
+
+            EditVM model = new EditVM();
+            model.PopulateModel(item);
+            PopulateEditVM(model);
+
+            return PartialView("~/Views/Partials/_EditUser.cshtml", model);
+        }
+
+        [HttpPost]
+        public override ActionResult Edit(EditVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return PartialView("~/Views/Partials/_EditUser.cshtml", model);
+            }
+
+            UsersRepository repo = new UsersRepository();
+            User item = new User();
+            model.PopulateEntity(item);
+
+            repo.Save(item);
+            model.Id = item.Id;
+
+            return Content("");
+        }
     }
 }
---
+++ Reddit/Scripts/edit-modal.js
@@ -0,0 +1,37 @@
+﻿function editErrors() {
+
+    const modal = document.getElementsByClassName('edit-modal');
+
+    if (modal.length > 0) {
+        var form = $(".edit-modal");
+        form.removeData('validator');
+        form.removeData('unobtrusiveValidation');
+        $.validator.unobtrusive.parse(form);
+
+        modal[0].classList.add('active');
+    }
+    else {
+        window.location.reload();
+    }
+
+}
+
+var triggerEditModal = () => {
+    $(".edit-btn").click(function (e) {
+        e.preventDefault();
+        var id = $(this).data('id');
+        var action = $(this).data('action');
+        $.get(`${action}`, { id: id != null ? id : null })
+            .done(function (data) {
+                $("#edit-container").empty().html(data);
+                $(".edit-modal").addClass("active");
+            });
+    });
+
+    $(document).on('click', '.edit-modal-close', function () {
+        $(".edit-modal").removeClass("active");
+    });
+}
+
+window.addEventListener('DOMContentLoaded', triggerEditModal, false);
+window.addEventListener('DOMNodeInserted', triggerEditModal, false);
