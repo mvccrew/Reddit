1ea56eb198699367e873dfc3de4600b73fc47640
Yovcho <ivelinjilov16@gmail.com>
2019-04-19 18:14:09 +0300

    Can add videos now.Max file format is 1GB now

---
+++ Reddit/FileFormats/Formats.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reddit.FileFormats
+{
+    public class Formats
+    {
+        public static List<string> VideoFormats
+        {
+            get
+            {
+                return new List<string> { "mp4", "webm", "mkv", "flv", "vob", "ogv",
+                    "ogg", "drc", "gif", "gifv", "mng", "avi", "mov", "qt", "wmv", "yuv", "rm",
+                "rmvb","asf","amv","m4p","m4v","mpg","mp2","mpeg","mpe","mpv","mpg","m2v","m4v",
+                "svi","3gp","3g2","mxf","roq","nsv","f4v","f4p","f4a","f4b"};
+
+            }
+        }
+    }
+}
