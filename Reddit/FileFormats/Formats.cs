7444e0253d52bc49d60fcc753d1065df30f3ebd3
Daniel Ivanov <ducciito@hotmail.com>
2019-04-21 17:01:31 +0300

    minor styling fixes

--- Reddit/FileFormats/Formats.cs
+++ Reddit/FileFormats/Formats.cs
@@ -12,7 +12,7 @@ namespace Reddit.FileFormats
             get
             {
                 return new List<string> { "mp4", "webm", "mkv", "flv", "vob", "ogv",
-                    "ogg", "drc", "gif", "gifv", "mng", "avi", "mov", "qt", "wmv", "yuv", "rm",
+                    "ogg", "drc", "mng", "avi", "mov", "qt", "wmv", "yuv", "rm",
                 "rmvb","asf","amv","m4p","m4v","mpg","mp2","mpeg","mpe","mpv","mpg","m2v","m4v",
                 "svi","3gp","3g2","mxf","roq","nsv","f4v","f4p","f4a","f4b"};
 
@@ -22,7 +22,7 @@ namespace Reddit.FileFormats
         {
             get
             {
-                return new List<string> {"tif","tiff","jpeg","jpg","jif","jfif","jp2","jpx","j2k","j2c","fpx","pcd","png" };
+                return new List<string> {"gif","tif","tiff","jpeg","jpg","jif","jfif","jp2","jpx","j2k","j2c","fpx","pcd","png" };
             }
         }
     }
