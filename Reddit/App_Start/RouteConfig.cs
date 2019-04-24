7367fc603acc9f4f8ad225d3ec22a078b1d0582b
Yovcho <ivelinjilov16@gmail.com>
2019-04-24 22:58:14 +0300

    More routes

--- Reddit/App_Start/RouteConfig.cs
+++ Reddit/App_Start/RouteConfig.cs
@@ -17,9 +17,11 @@ namespace Reddit
 
             SubRedditsRepository repo = new SubRedditsRepository();
             PostsRepository postsRepository = new PostsRepository();
+            UsersRepository usersRepository = new UsersRepository();
 
             List<SubReddit> items = repo.GetAll(null);
             List<Post> posts = postsRepository.GetAll(null);
+            List<User> users = usersRepository.GetAll(null);
             foreach (SubReddit sr in items)
             {
                 routes.MapRoute(
@@ -34,12 +36,38 @@ namespace Reddit
                     defaults: new { controller = "Comments", action = "Index", PostId = item.Id });
                 }
             }
+            foreach (User item in users)
+            {
+                routes.MapRoute(
+                name: "Profile" + item.Id,
+                url: "user/" + item.Username,
+                defaults: new { controller = "Profile", action = "Index", UserId = item.Id }
+                 );
+            }
 
+            /*foreach(var item in Reddit.Models.AuthManager.LoggedUser.SubscribedToSubReddits)
+            {
+                routes.MapRoute(
+                name: "PostsCreate",
+                url: "r/" + item.Name + "/submit"  ,
+                defaults: new { controller = "Posts", action = "Create", id = UrlParameter.Optional }
+                 );
+            }
+
+            string allOrHome = "";
+            if (Reddit.Models.AuthManager.LoggedUser != null) allOrHome = "r/home";
+            else allOrHome = "r/all";
+            routes.MapRoute(
+               name: "allHome",
+               url: allOrHome,
+               defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+           );*/
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional}
             );
+
         }
         public static string getTitleForURL(string title)
         {
