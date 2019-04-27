d5bdcfdfdb815f4d6aa9f325d9363d7a1db9b3ff
Daniel Ivanov <ducciito@hotmail.com>
2019-04-27 20:39:36 +0300

    tidy up the create posts view

---
+++ Reddit/Scripts/post-tabs.js
@@ -0,0 +1,29 @@
+﻿function onTabClick(event) {
+    let activeTabs = document.querySelectorAll('.active');
+    let clickedTab = event.target.id;
+
+    activeTabs.forEach(function (tab) {
+        tab.className = tab.className.replace('active', '');
+    });
+
+    event.target.parentElement.className += ' active';
+
+    if (clickedTab == 'text') {
+        document.querySelector('#PostTypeId').value = "1";
+        document.querySelector('.content').innerHTML =
+            '<textarea class="form-input" cols="20" id="Content" name="Content" placeholder="Text (optional)" rows="3" type=""></textarea>';
+    }
+    else if (clickedTab == 'link') {
+        document.querySelector('#PostTypeId').value = "3";
+        document.querySelector('.content').innerHTML =
+            '<textarea class="form-input" cols="20" id="Content" name="Content" placeholder="Url" rows="3" type=""></textarea>';
+    }
+    else {
+        document.querySelector('#PostTypeId').value = "2";
+        document.querySelector('.content').innerHTML =
+            '<input class="form-input" name="image" type="file"/>';
+    }
+}
+
+const element = document.getElementById('post-tab');
+element.addEventListener('click', onTabClick, false);
